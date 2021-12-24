b6ab0430fc368fce9ba283e00f8e5ee0cc24a16e
joseandre <contretaa@gmail.com>
2021-12-24 16:11:14 -0400

    added solution day 22

---
+++ day22.js
@@ -0,0 +1,57 @@
+function countDecoration(bigTree) {
+  return walkTree(bigTree);
+}
+
+function walkTree(node) {
+  let count;
+  if (node == null) {
+    count = 0;
+  } else {
+    count =
+      node.value + countDecoration(node.left) + countDecoration(node.right);
+  }
+  return count;
+}
+const tree = {
+  value: 1, // el nodo raíz siempre es uno, porque es la estrella ⭐
+  left: {
+    value: 2, // el nodo izquierdo necesita dos decoraciones
+    left: null, // no tiene más ramas
+    right: null, // no tiene más ramas
+  },
+  right: {
+    value: 3, // el nodo de la derecha necesita tres decoraciones
+    left: null, // no tiene más ramas
+    right: null, // no tiene más ramas
+  },
+};
+const bigTree = {
+  value: 1,
+  left: {
+    value: 5,
+    left: {
+      value: 7,
+      left: {
+        value: 3,
+        left: null,
+        right: null,
+      },
+      right: null,
+    },
+    right: null,
+  },
+  right: {
+    value: 6,
+    left: {
+      value: 5,
+      left: null,
+      right: null,
+    },
+    right: {
+      value: 1,
+      left: null,
+      right: null,
+    },
+  },
+};
+binaryTree(bigTree);
